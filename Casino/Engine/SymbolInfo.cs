5dcb152a212fd20b423026b80c11be66876ad7cf
hajn1e <thexhaj888@gmail.com>
2025-11-19 10:36:53 +0100

    Add project files.

---
+++ Casino/Engine/SymbolInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Engine
+{
+    public class SymbolInfo
+    {
+        public static (string Labe, Color Background, Color Foreground) Get(Symbol s)
+        {
+            switch (s)
+            {
+                case Symbol.Cherry:
+                    return ("Cseresznye", Color.Red, Color.White);
+                case Symbol.Lemon:
+                    return ("Citrom", Color.Yellow, Color.Black);
+                case Symbol.Bar:
+                    return ("BAR", Color.Gray, Color.White);
+                case Symbol.Diamond:
+                    return("Gyémánt", Color.LightBlue, Color.Black);
+                case Symbol.Seven:
+                    return ("777", Color.Green, Color.Black);
+                default:
+                    return("?", Color.DarkGray, Color.White);
+
+            }
+        }
+    }
+}
